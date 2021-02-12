b11bd9ac323bf8e30f600bde793f5ff7002a4e37
Berenas54 <haravoy1998@mail.ru>
2021-02-12 21:04:39 +0300

    74 task , synchronization status

--- src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.tsx
@@ -26,6 +26,14 @@ export class ProfileStatus extends React.Component<any> {
         })
     }
 
+//пофиксить any внизу, не знал как типизировать
+    componentDidUpdate(prevProps: any, prevState: any) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            })
+        }
+    }
 
     render() {
         return (
