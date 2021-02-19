4650d6a9c74ab20a42d54ed7eb7236fbbeda8279
Berenas54 <haravoy1998@mail.ru>
2021-02-19 23:47:10 +0300

    78 fixed

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-import {InjectedFormProps, reduxForm, Field} from "redux-form";
+import {Field, InjectedFormProps, reduxForm} from "redux-form";
 import {Input} from "../commons/FormsControls/FormsControls";
 import {required} from "../../utils/validators/validators";
 import {connect} from "react-redux";
-import {AuthType, login} from "../../redux/auth-reducer";
+import {login} from "../../redux/auth-reducer";
+import {Redirect} from "react-router-dom";
+import {ReduxRootStateType} from "../../redux/redux-store";
 
 type FormDataType = {
     email: string,
@@ -18,16 +20,18 @@ type MapDispatchPropsType = {
 }
 
 //fix
-const Login:React.FC<MapStatePropsType & MapDispatchPropsType> = (props) => {
+const Login: React.FC<MapStatePropsType & MapDispatchPropsType> = (props) => {
     const onSubmit = (formData: FormDataType) => {
         props.login(formData.email, formData.password, formData.rememberMe)
     }
-    if (props.isAuth)
-        return (<div>
-                <h1>LOGIN</h1>
-                <LoginReduxForm onSubmit={onSubmit}/>
-            </div>
-        )
+    if (props.isAuth) {
+        return <Redirect to={"/profile"}/>
+    }
+    return (<div>
+            <h1>LOGIN</h1>
+            <LoginReduxForm onSubmit={onSubmit}/>
+        </div>
+    )
 }
 
 export const LoginForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
@@ -55,8 +59,8 @@ const LoginReduxForm = reduxForm<FormDataType>({
     form: 'Login'
 })(LoginForm)
 
-const mapStateToProps = (state: AuthType):MapStatePropsType => ({
-    isAuth: state.isAuth
+const mapStateToProps = (state:ReduxRootStateType): MapStatePropsType => ({
+    isAuth: state.auth.isAuth
 })
 
 export default connect(mapStateToProps, {login})(Login)
